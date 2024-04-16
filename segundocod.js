1ddfda668304575a7e96ed78faf278a7cb1bd2ca
friccxs <167122311+friccxs@users.noreply.github.com>
2024-04-16 17:05:10 -0300

    Create segundocod.js
    Meu segundo codigo agora com interação

---
+++ segundocod.js
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>JavaScript</title>
+    <style>
+body {
+     font-family: Arial;
+     font-size: 15px;
+     background-color: black;
+     color: white;
+
+}
+
+
+    </style>
+</head>
+<body>
+    <h1>Aprendendo A Tratar Dados Com JavaScript</h1>
+    <p>Irei usar o prompt para guardar alguns dados de navegação.</p>
+
+    <script>
+   var nome = prompt('Qual seu nome?') // vai perguntar o nome em uma barra em cima usando o navedador
+   window.alert('Seus dados foram armazenados em nosso sistema, ' + nome + '! Espero que aproveite de nossos serviços e seu tempo conosco.') // Serve para concatenação
+    </script>
+    
+</body>
+</html>
